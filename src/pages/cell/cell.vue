e6378fb6cc3c2065ba39274a6cfb52ba7054a192
erhui <1551432132@qq.com>
2018-05-30 11:18:37 +0800

    vue多页面应用

---
+++ src/pages/cell/cell.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>我是cell页面</div>
+</template>
+
+<script>
+	export default {
+    name: 'app',
+    mounted () {
+    	
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ 新建文件夹/vue-/src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<p>我是index页面</p>
+		<a href='cell.html'>去cell页面</a>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    mounted () {
+    	
+    }
+  }
+</script>
+
+<style>
+</style>
